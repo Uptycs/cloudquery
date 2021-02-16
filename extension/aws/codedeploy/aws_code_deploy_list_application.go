969640c88c6e886d8ba2c520809f9a799379c25d
Manish Kumar <mkumar@uptycs.com>
2021-02-16 19:33:50 +0530

    ported aws_codedeploy

--- extension/aws/codedeploy/aws_code_deploy_list_application.go
+++ extension/aws/codedeploy/aws_code_deploy_list_application.go
@@ -18,10 +18,10 @@ import (
 
 	"github.com/Uptycs/cloudquery/utilities"
 
+	"github.com/Uptycs/basequery-go/plugin/table"
 	extaws "github.com/Uptycs/cloudquery/extension/aws"
 	"github.com/aws/aws-sdk-go-v2/service/codedeploy"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/kolide/osquery-go/plugin/table"
 )
 
 // ListApplicationsColumns returns the list of columns in the table
@@ -145,7 +145,9 @@ func processAccountListApplications(account *utilities.ExtensionConfigurationAws
 	for _, region := range regions {
 		result, err := processRegionListApplications(tableConfig, account, region)
 		if err != nil {
-			return resultMap, err
+			utilities.GetLogger().WithFields(log.Fields{
+				"tableName": "aws_code_deploy_list_application",
+			}).Error(resultMap, err)
 		}
 		resultMap = append(resultMap, result...)
 	}
